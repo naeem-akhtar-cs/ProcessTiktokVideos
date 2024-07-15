0e1f09dc72d066370885030a3d8317176c346ad9
Naeem Akhtar <naeem.akhtar.cs@gmail.com>
2024-07-15 08:30:22 +0500

    Rotate video

---
+++ README.md
@@ -0,0 +1,23 @@
+i didn't get it to look good with:
+
+-vf "rotate=3*PI/180, crop=iw-2*abs(iw*sin(3*PI/180)), scale=iw:ih, pad=ceil(iw/2)*2:ceil(ih/2)*2:(ceil(iw/2)*2-iw)/2:(ceil(ih/2)*2-ih)/2"
+
+(had still black borders)
+
+
+
+here is the download of original video and its variant 5 (rotated 3 degrees, ignore brightness etc)
+to compare how i like it to look like.
+
+i have the variants from an third party software and trying to build our own now with you.
+
+
+
+original video:
+
+https://drive.google.com/file/d/10DVom-KNVBOPZRzzs1uTjIIJenugAEnK/view?usp=drivesdk
+
+Variant 5 (old third party software)
+
+
+https://fansgrowth.pro/unique-content/d979e265-7b49-4551-a66c-b9bf248c9aff/IMG_2521.MOV
--- index.py
+++ index.py
@@ -1,15 +1,11 @@
-import requests, json
-import os
+import requests, json, subprocess, os
 from dotenv import load_dotenv
-
-from datetime import datetime, timedelta
-import urllib.parse
 
 load_dotenv()
 
 AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
 AIRTABLE_BASE_ID = os.getenv('AIRTABLE_BASE_ID')
-AIRTABLE_TABLE_ID = os.getenv('AIRTABLE_TABLE_NAME')
+AIRTABLE_TABLE_ID = os.getenv('AIRTABLE_TABLE_ID')
 AIRTABLE_VIEW_ID = os.getenv('AIRTABLE_VIEW_ID')
 
 baseUrl = 'https://api.airtable.com/v0'
@@ -57,14 +53,36 @@ def downloadVideo(videoUrl, folderName, recordId):
     print(f"Video: {fileName} downloaded")
     return fileName
 
+
+def processVideo(downloadedVideos, processedVideos, fileName, processingSpecs):
+    ffmpegCommand = [
+        'ffmpeg',
+        '-i', f"{downloadedVideos}/{fileName}",
+        '-vf', f'rotate={processingSpecs["rotationAngle"]}*PI/180',
+        '-metadata:s:v:0', f'rotate={processingSpecs["rotationAngle"]}',
+        '-codec:a', 'copy',
+        f"{processedVideos}/{fileName}",
+    ]
+    
+    subprocess.run(ffmpegCommand, check=True)
+    print(f"{processedVideos}/{fileName} processed and saved")
+
+
 if __name__ == '__main__':
-    folderName = "videos"
+    downloadedVideos = "DownloadedVideos"
+    processedVideos = "ProcessedVideos"
+
     currentDirectory = os.getcwd()
-    folderPath = os.path.join(currentDirectory, folderName)
+
+    folderPath = os.path.join(currentDirectory, downloadedVideos)
     if not os.path.exists(folderPath):
         os.makedirs(folderPath)
-        print(f"Folder '{folderName}' created.")
+        print(f"Folder '{downloadedVideos}' created.")
 
+    folderPath = os.path.join(currentDirectory, processedVideos)
+    if not os.path.exists(folderPath):
+        os.makedirs(folderPath)
+        print(f"Folder '{processedVideos}' created.")
 
     offset = None
     firstRequest = True
@@ -76,10 +94,16 @@ if __name__ == '__main__':
 
         if records:
             print(f"Retrieved {len(records)} records from Airtable")
-            for record in records:
+            for record in records[:1]:
                 recordId = record["id"]
                 recordFields = record['fields']
-                fileName = downloadVideo(recordFields["Google Drive URL"], folderName, recordId)
+                fileName = downloadVideo(recordFields["Google Drive URL"], downloadedVideos, recordId)
+
+                processingSpecs = {
+                    "rotationAngle": 3
+                }
+
+                processVideo(downloadedVideos, processedVideos, fileName, processingSpecs)
         else:
             print("No records retrieved from AirTable")
         firstRequest = False

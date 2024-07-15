ae1b0dc810b4e5b8bfd07ffb2e68c2d06bfbc2ed
Naeem Akhtar <naeem.akhtar.cs@gmail.com>
2024-07-15 07:37:04 +0500

    Download video from drive

--- index.py
+++ index.py
@@ -2,6 +2,9 @@ import requests, json
 import os
 from dotenv import load_dotenv
 
+from datetime import datetime, timedelta
+import urllib.parse
+
 load_dotenv()
 
 AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
@@ -12,18 +15,28 @@ AIRTABLE_VIEW_ID = os.getenv('AIRTABLE_VIEW_ID')
 baseUrl = 'https://api.airtable.com/v0'
 
 
-def getAirtableRecords():
+def getAirtableRecords(offset):
     url = f"{baseUrl}/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_ID}"
     headers = {'Authorization': f'Bearer {AIRTABLE_API_KEY}'}
+
     params = {
         "view": AIRTABLE_VIEW_ID
     }
+    if offset is not None:
+        params["offset"] = offset
+
+    print(f"Records Offset: {offset}")
+
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, params = params)
         response.raise_for_status()
         
         data = response.json()
-        return data.get('records', [])
+        airtableData  = {
+            "records": data.get('records', []),
+            "offset": data.get("offset")
+        }
+        return airtableData
 
     except requests.exceptions.HTTPError as e:
         print(f"HTTP Error: {e}")
@@ -34,13 +47,39 @@ def getAirtableRecords():
         print(f"Request Exception: {e}")
         return []
 
+
+def downloadVideo(videoUrl, folderName, recordId):
+    fileName = f"{recordId}.mp4"
+    response = requests.get(url = videoUrl, stream=True)
+    with open(f"{folderName}/{fileName}", "wb") as writer:
+        for chunk in response.iter_content(chunk_size=8192):
+            writer.write(chunk)
+    print(f"Video: {fileName} downloaded")
+    return fileName
+
 if __name__ == '__main__':
-    
-    records = getAirtableRecords()
-    
-    if records:
-        print(f"Retrieved {len(records)} records from Airtable")
-        for record in records[:5]:
-            print(record['fields'])
-    else:
-        print("No records retrieved. Check the error messages above.")
+    folderName = "videos"
+    currentDirectory = os.getcwd()
+    folderPath = os.path.join(currentDirectory, folderName)
+    if not os.path.exists(folderPath):
+        os.makedirs(folderPath)
+        print(f"Folder '{folderName}' created.")
+
+
+    offset = None
+    firstRequest = True
+
+    while offset is not None or firstRequest:
+        data = getAirtableRecords(offset)
+        records = data["records"]
+        offset = data["offset"]
+
+        if records:
+            print(f"Retrieved {len(records)} records from Airtable")
+            for record in records:
+                recordId = record["id"]
+                recordFields = record['fields']
+                fileName = downloadVideo(recordFields["Google Drive URL"], folderName, recordId)
+        else:
+            print("No records retrieved from AirTable")
+        firstRequest = False
